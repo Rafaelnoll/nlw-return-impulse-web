d1caaf94aaeb506648a057fc7a622bf8e0f55ee4
Rafael Noll <rafanoll2004@gmail.com>
2022-05-14 11:37:57 -0300

    adicionando variável de ambiente PORT

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,11 +3,12 @@ import { routes } from "./routes";
 import cors from "cors";
 
 const app = express();
+const PORT = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(PORT, () => {
     console.log("HTTP server running");
 });
